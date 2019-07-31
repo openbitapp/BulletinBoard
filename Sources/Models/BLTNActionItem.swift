d4685dfe6abf37d4f9926a8f3ca25a9bd7eb93ba
Roberto Casula <rcasula3@gmail.com>
2019-07-31 16:30:40 +0200

    test with scrollview

--- Sources/Models/BLTNActionItem.swift
+++ Sources/Models/BLTNActionItem.swift
@@ -296,7 +296,19 @@ open class BLTNActionItem: NSObject, BLTNItem {
         let interfaceBuilder = interfaceBuilderType.init(appearance: appearance, item: self)
 
         let contentViews = makeContentViews(with: interfaceBuilder)
-        arrangedSubviews.append(contentsOf: contentViews)
+//        arrangedSubviews.append(contentsOf: contentViews)
+
+        let contentViewsStack = interfaceBuilder.makeGroupStack(spacing: 10)
+
+        contentViews.forEach { view in
+            contentViewsStack.addArrangedSubview(view)
+        }
+
+        let scrollView = UIScrollView(frame: .zero)
+
+        scrollView.addSubview(contentViewsStack)
+
+        arrangedSubviews.append(scrollView)
 
         // Buttons stack
 
