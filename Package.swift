e871aa047016fb936943736b638ba069954cc45a
Roberto Casula <rcasula3@gmail.com>
2019-07-31 15:54:33 +0200

    fix library name

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "BLTNBoard",
-            targets: ["BulletinBoard"]
+            name: "BulletinBoard",
+            targets: ["BLTNBoard"]
         )
     ],
     dependencies: [
@@ -23,13 +23,13 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "BulletinBoard",
+            name: "BLTNBoard",
             dependencies: [],
             path: "Sources"
         ),
         .testTarget(
             name: "BulletinBoardTests",
-            dependencies: ["BulletinBoard"]
+            dependencies: ["BLTNBoard"]
         )
     ]
 )
